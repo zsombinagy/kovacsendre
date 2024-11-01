f60b2048fef1f0875ac6e5c1f420553f1d0fe845
zsombinagy <zsombinagy.14@gmail.com>
2024-11-01 12:14:36 +0100

    added semicircles to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,47 +14,68 @@ const Navbar = () => {
   return (
     <>
       <div className="w-full">
-        
-        <div className="bg-navbarBackground text-basicBackground md:justify-around flex mt-7 justify-center items-center py-3 w-full">
+        <div className="bg-navbarBackground text-basicBackground md:justify-around flex mt-12 justify-center items-center py-3 w-full">
           <div className="hidden md:flex text-sm">
-              <button className="mr-8 border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500"><Link href="/tanarokespeldakepek">Tanárok és Példaképek</Link></button>
-              <button className="border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500"><Link href="/muveszkollegak">Művészkollégák</Link></button>
+            <button className="mr-8 border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500">
+              <Link href="/tanarokespeldakepek">Tanárok és Példaképek</Link>
+            </button>
+            <button className="border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500">
+              <Link href="/muveszkollegak">Művészkollégák</Link>
+            </button>
           </div>
           <div className="flex flex-col justify-center items-center">
-              <Image
-                src="/orgona.png"
-                width={50}
-                height={50}
-                alt="Picture of the author"
-              ></Image>
-              <h1 className="text-xl font-semibold md:text-2xl md:mt-3 hover:text-white duration-300"><button><Link href="/">Kovács Endre</Link></button></h1>
-              <h2 className="text-[0.6rem] font-thin mb-3 md:text-sm">
-                orgonaművész emlékoldala
-              </h2>
-              <button onClick={handleToggle} className="focus:outline-none md:hidden bg-basicBackground px-2 py-1 text-navbarBackground text-base rounded-md hover:bg-navbarBackground hover:text-basicBackground duration-500 border-2 border-basicBackground"> Menu</button>
+            <div className="absolute top-[1.7rem] w-28 h-20 bg-navbarBackground rounded-t-full"></div>
+            <Image
+              src="/orgona.png"
+              width={50}
+              height={50}
+              alt="Picture of the author"
+              className="z-10"
+            ></Image>
+            <h1 className="text-xl font-semibold md:text-2xl md:mt-3 hover:text-white duration-300 z-10">
+              <button>
+                <Link href="/">Kovács Endre</Link>
+              </button>
+            </h1>
+            <h2 className="text-[0.6rem] font-thin mb-3 md:text-sm z-10">
+              orgonaművész emlékoldala
+            </h2>
+            <button onClick={handleToggle} className="focus:outline-none md:hidden bg-basicBackground px-2 py-1 text-navbarBackground text-base rounded-md hover:bg-navbarBackground hover:text-basicBackground duration-500 border-2 border-basicBackground z-10"  >
+              Menu
+            </button>
+            <div className={`absolute ${isToggled ? "hidden" : "top-[9.5rem]"} duration-500 md:top-[8.5rem] w-28 h-20 bg-navbarBackground rounded-b-full -z-10`}></div>
           </div>
+          
           <div className="hidden md:block text-sm">
-              <button className="mr-8 border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500"><Link href="/ujsagcikkek">Újságcikkek, kritikák, elismerések</Link></button>
-              <button className="border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500"><Link href="/kapcsolat">Kapcsolat</Link></button>
+            <button className="mr-8 border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500">
+              <Link href="/ujsagcikkek">
+                Újságcikkek, kritikák, elismerések
+              </Link>
+            </button>
+            <button className="border-2 border-basicBackground rounded p-1 hover:bg-basicBackground hover:text-navbarBackground duration-500">
+              <Link href="/kapcsolat">Kapcsolat</Link>
+            </button>
           </div>
-
         </div>
 
-        <div className={`flex flex-col justify-center bg-navbarBackground items-center md:hidden transition-all duration-500 ease-in-out transform${
-              isToggled ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0" }`}          >
-            <div className="w-full border-basicBackground border-t-2 justify-center flex">
-              <h1>Tanárok és példaképek</h1>
-            </div>
-            <div className="w-full border-basicBackground border-t-2 justify-center flex">
-              <h1>Pályatársak és művészkollégák</h1>
-            </div>
-            <div className="w-full border-basicBackground border-t-2 justify-center flex">
-              <h1>Újságcikkek, kritikák, elismerések</h1>
-            </div>
-            <div className="w-full border-basicBackground border-y-2 justify-center flex">
-              <h1>Kapcsolat</h1>
-            </div>
+        <div
+          className={`flex flex-col justify-center bg-navbarBackground items-center md:hidden transition-all duration-500 ease-in-out transform${
+            isToggled ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"} z-10`}>
+          <div className="w-full border-basicBackground border-t-2 justify-center flex">
+            <h1>Tanárok és példaképek</h1>
           </div>
+          <div className="w-full border-basicBackground border-t-2 justify-center flex">
+            <h1>Pályatársak és művészkollégák</h1>
+          </div>
+          <div className="w-full border-basicBackground border-t-2 justify-center flex">
+            <h1>Újságcikkek, kritikák, elismerések</h1>
+          </div>
+          <div className="w-full border-basicBackground border-t-2 justify-center flex">
+            <h1>Kapcsolat</h1>
+          </div>
+          <div className={`absolute top-[16.3rem] w-28 h-20 bg-navbarBackground rounded-b-full -z-10`}></div>
+
+        </div>
       </div>
     </>
   );
